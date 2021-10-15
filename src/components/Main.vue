7edd673ab82766faffe8005c9083f8645c1e642f
dOctorWild <jareddavidwild4@gmail.com>
2021-10-15 10:26:43 +0200

    Setup general layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>This is my custom Template/Component</h1>
+  <section id="main" class="main">
+    <img src="../assets/images/bg-desktop-dark.jpg" class="background-img">
+    <div class="body">
+      <section class="container">
+        <div class="heading">
+          <h1>TODO</h1>
+          <button><img src="../assets/images/icon-sun.svg"></button>
+        </div>
+
+        <div class="create">
+          <h1>Create a todo...</h1>
+        </div>
+        <div class="todos">
+          <h1>This is a todo b*tch</h1>
+        </div>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
