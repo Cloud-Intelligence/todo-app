672184669c5c1f1f79f01ce645f55a4299c656be
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-10-18 11:55:22 +0200

    can empty completed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,19 +12,18 @@
           <h1>Create a todo...</h1>
         </div>
         <div class="todos">
-          <div class="todo container">
+          <div v-for="todoItem in todoItems" class="todo container">
             <button class="checkbox"><img src="../assets/images/icon-check.svg"></button>
-            <p class="text">Todo</p>
+            <p class="text">{{ todoItem }}</p>
           </div>
-          <hr>
           <div class="footer container">
             <p>5 Items left</p>
-            <div class="display-select">
+            <div class="footer-toes">
               <button>All</button>
               <button>Active</button>
               <button>Completed</button>
             </div>
-            <p>Clear completed</p>
+            <button @click="clearCompleted">Clear Completed</button>
           </div>
         </div>
       </section>
@@ -34,7 +33,18 @@
 
 <script>
 export default {
-  name: "Main"
+  name: "Main",
+  data() {
+    return {
+      todoItems: ['Complete JavaScript course', 'Gor for 10 minute run', 'Drink a whole bottle of tequila'],
+      completedItems: ['billy the bee']
+    }
+  },
+  methods: {
+    clearCompleted() {
+      this.completedItems = [];
+    }
+  }
 }
 </script>
 
