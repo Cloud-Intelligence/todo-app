db3c8106ad36dd108d338e1647939f3907547455
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-10-18 10:44:07 +0200

    setup the general layout of how the apps gonna look

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <section id="main" class="main">
     <img src="../assets/images/bg-desktop-dark.jpg" class="background-img">
     <div class="body">
-      <section class="container">
+      <section class="body-container">
         <div class="heading">
           <h1>TODO</h1>
           <button><img src="../assets/images/icon-sun.svg"></button>
@@ -12,7 +12,20 @@
           <h1>Create a todo...</h1>
         </div>
         <div class="todos">
-          <h1>This is a todo b*tch</h1>
+          <div class="todo container">
+            <button class="checkbox"><img src="../assets/images/icon-check.svg"></button>
+            <p class="text">Todo</p>
+          </div>
+          <hr>
+          <div class="footer container">
+            <p>5 Items left</p>
+            <div class="display-select">
+              <button>All</button>
+              <button>Active</button>
+              <button>Completed</button>
+            </div>
+            <p>Clear completed</p>
+          </div>
         </div>
       </section>
     </div>
