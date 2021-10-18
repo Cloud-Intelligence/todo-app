8d1c9e8cf5cd9f3dddbabfa026bfc7c414bc4aeb
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-10-18 13:15:24 +0200

    finished dark theme layout and functionality. all data is stored in session storage. users can select done todo. user can now add a new todo. users can now view all their completed todos. users can view all their uncomplete todos. jimmy was there, it was great. no krunker :(

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,20 +8,26 @@
           <button><img src="../assets/images/icon-sun.svg"></button>
         </div>
 
-        <div class="create">
-          <h1>Create a todo...</h1>
+        <div class="create container" @click="addTodo = true">
+          <button @click="addNewTodoItem" class="checkbox"><img src="../assets/images/icon-check.svg"></button>
+          <h1 v-if="!addTodo">Create a todo...</h1>
+          <input v-else @keyup.enter="addNewTodoItem" class="input" ref="todoItem">
         </div>
         <div class="todos">
-          <div v-for="todoItem in todoItems" class="todo container">
-            <button class="checkbox"><img src="../assets/images/icon-check.svg"></button>
+          <div v-if="showTodos || showAll" v-for="(todoItem, index) in todoItems" class="todo container">
+            <button class="checkbox" @click="markCompleted(index)"></button>
             <p class="text">{{ todoItem }}</p>
           </div>
+          <div v-if="!showTodos || showAll" v-for="(completedItem, index) in completedItems" class="completed container">
+            <button class="checkbox" @click="unmarkCompleted(index)"><img src="../assets/images/icon-check.svg"></button>
+            <p class="text">{{ completedItem }}</p>
+          </div>
           <div class="footer container">
-            <p>5 Items left</p>
+            <p>{{ todoItems.length }} Items left</p>
             <div class="footer-toes">
-              <button>All</button>
-              <button>Active</button>
-              <button>Completed</button>
+              <button @click="showAll = true" :style="showAll ? 'color: var(--bright-blue)' : ''">All</button>
+              <button @click="showActive" :style="showTodos && !showAll ? 'color: var(--bright-blue)' : ''">Active</button>
+              <button @click="showCompleted" :style="!showTodos && !showAll ? 'color: var(--bright-blue)' : ''">Completed</button>
             </div>
             <button @click="clearCompleted">Clear Completed</button>
           </div>
@@ -36,14 +42,53 @@ export default {
   name: "Main",
   data() {
     return {
-      todoItems: ['Complete JavaScript course', 'Gor for 10 minute run', 'Drink a whole bottle of tequila'],
-      completedItems: ['billy the bee']
+      todoItems: [],
+      completedItems: [],
+      showTodos: true,
+      showAll: false,
+      addTodo: false,
     }
   },
+  mounted() {
+    this.todoItems = JSON.parse(window.localStorage.getItem('todoItems')) ?? [];
+    this.completedItems = JSON.parse(window.localStorage.getItem('completedItems')) ?? [];
+  },
   methods: {
+    markCompleted(index) {
+      this.completedItems.push(this.todoItems[index]);
+      this.todoItems.splice(index, 1);
+      this.updateLocalStorage();
+    },
+    unmarkCompleted(index) {
+      this.todoItems.push(this.completedItems[index]);
+      this.completedItems.splice(index, 1);
+      this.updateLocalStorage();
+    },
     clearCompleted() {
       this.completedItems = [];
-    }
+      this.updateLocalStorage();
+    },
+    showActive() {
+      this.showTodos = true;
+      this.showAll = false;
+    },
+    showCompleted() {
+      this.showAll = false;
+      this.showTodos = false;
+    },
+    addNewTodoItem() {
+      const item = this.$refs['todoItem'].value;
+      if (item === '') return;
+
+      this.todoItems.push(item);
+      this.$refs['todoItem'].value = '';
+      this.addTodo = false;
+      this.updateLocalStorage();
+    },
+    updateLocalStorage() {
+      window.localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
+      window.localStorage.setItem('completedItems', JSON.stringify(this.completedItems));
+    },
   }
 }
 </script>
